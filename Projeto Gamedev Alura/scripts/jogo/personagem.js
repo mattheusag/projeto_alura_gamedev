64cd9eb52ec39e45f74314ca90c890416f83cf39
Mattheus Gomes <macttheusag@yahoo.com.br>
2020-07-02 11:39:46 -0300

    commit Pulo, Efeitos sonoros, Colisão e Correção de bug de som

--- Projeto Gamedev Alura/scripts/jogo/personagem.js
+++ Projeto Gamedev Alura/scripts/jogo/personagem.js
@@ -1,31 +1,41 @@
 
-class Personagem {
-    constructor(imagem){
-        this.imagem = imagem
-        this.matriz = [ 
-            [0,0],  [220,0],  [440,0], [660,0],
-            [0,270], [220,270], [440,270], [660,270],
-            [0,540], [220,540], [440,540], [660,540],
-            [0,810], [220,810], [440,810], [660,810]
-          ]
-          // essa matriz é o mapeamento da imagem da personagem. A imagem da personagem é composta de 16 figuras diferentes que simulam a movimentação. 
+class Personagem extends Animacao {
+    constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite){
+        super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite)
 
-          this.frameAtual = 0 
+        this.yInicial = height - this.altura
+        this.y = this.yInicial 
+
+        this.velocidadeDoPulo = 0 
+        this.gravidade = 3 
+
     }
 
-    exibe(){
-        image(this.imagem, 0, height - alturaDaPersonagem , 110, alturaDaPersonagem, this.matriz[this.frameAtual][0], this.matriz[this.frameAtual][1], 220, 270) /* os parâmetros são: a variavel, as coordenadas, e os tamanhos, posição x, y mapeada - Pegue a imagem, vá no ponto 0 e (height - altura), coloque 110 e 135 a partir desse ponto, e coloque na tela no ponto 0, 0, no tamanho 220 x 270.
-        Para que haja movimentação, não podemos usar mais 0,0, e sim, valores referentes a matriz. 
-        Explicação do height - altura : Ferramenta do p5.js , conseguimos pegar a altura da imagem, e o topo da cabeça da menina é o ponto 0, por isso precisamos dar -135 pois é a altura dela. Com isso ela fica na posição correta (no chão)*/
-
-        this.anima()
+    pula(){
+        this.velocidadeDoPulo =  -30
     }
 
-    anima (){
-        this.frameAtual++ // adiciona 1 ao frame atual 
+    aplicaGravidade(){
+        this.y = this.y + this.velocidadeDoPulo 
+        this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade
 
-        if(this.frameAtual >= this.matriz.length - 1){ // para limitar e não ultrapassar o maximo de imagens que temos
-            this.frameAtual = 0 // reseta para o 0
+        if(this.y > this.yInicial){ // para que ele nao caia infinitamente
+            this.y = this.yInicial //garanto com isso qu toda vez que meu y passar de this.yInicial, ele volte para this.yInicial 
         }
+    }
+// x da personagem, y da personagem, largura e altura do personagem
+    estaColidindo(inimigo){
+        const precisao = .65 // o hitbox dos dois é maior do que o tamanho reald eles, para corrigir isso precisamos aplicar essa constante, original é .7
+        const colisao = collideRectRect(
+            this.x, 
+            this.y,
+            this.largura * precisao, 
+            this.altura * precisao, 
+            inimigo.x, 
+            inimigo.y, 
+            inimigo.largura * precisao, 
+            inimigo.altura * precisao
+        )
+        return colisao
     }
 }
--- Projeto Gamedev Alura/sketch.js
+++ Projeto Gamedev Alura/sketch.js
@@ -36,33 +36,60 @@ const matrizInimigo = [
 	[208, 626],
 	[312, 626],
   ]
+const matrizPersonagem = this.matriz = [ 
+	[0,0],  [220,0],  [440,0], [660,0],
+	[0,270], [220,270], [440,270], [660,270],
+	[0,540], [220,540], [440,540], [660,540],
+	[0,810], [220,810], [440,810], [660,810]
+  ]
+  // essa matriz é o mapeamento da imagem da personagem. A imagem da personagem é composta de 16 figuras diferentes que simulam a movimentação. 
 
- // o loadImage é uma função do p5.js , conseguimos acessar por conta do nosso acesso a biblioteca no index.html
+ // o loadImage e loadSound são funções do p5.js , conseguimos acessar por conta do nosso acesso a biblioteca no index.html
 function preload() {
 	imagemCenario = loadImage ('imagens/cenario/floresta.png')
 	imagemPersonagem = loadImage ('imagens/personagem/correndo.png')
-	// somdojogo = loadSound('sons/trilha.mp3') // Não estou conseguindo carregar o som adequadamente.
+	somDoPulo = loadSound('sons/somPulo.mp3') 
+	somDoJogo = loadSound('sons/trilha_jogo.mp3')
+
 	imagemInimigo = loadImage ('imagens/inimigos/gotinha.png')
 }
 
 function setup() {
 	createCanvas(windowWidth, windowHeight)
 	cenario = new Cenario(imagemCenario, 3) // aqui é minha velocidade
-	// somdojogo.play()
-	personagem = new Personagem(imagemPersonagem)
+
+	personagem = new Personagem(matrizPersonagem, imagemPersonagem, 0, 110, 135, 220, 270)
 	inimigo = new Inimigo(matrizInimigo, imagemInimigo, width-52, 52, 52, 104, 104)  // instanciar 
 	frameRate(40) // velocidade de carregamento 
+	
   }
+
+  function keyPressed() { // função observadora nativa do p5 
+	  if (key === 'ArrowUp') {
+		  personagem.pula()
+		  somDoPulo.play()
+	  } else if (key === 'm') { // Minha alteração no projeto para tocar musica apenas se pressionar a tecla m
+		somDoJogo.play()
+	} else if (key === 'n') { // Mutar o jogo com a tecla n
+		somDoJogo.stop()
+	}
+
+ }
   
   function draw() {
 	cenario.exibe() // chamar exibe
 	cenario.move() // chamar move
 
 	personagem.exibe()
+	personagem.aplicaGravidade()
+
 	inimigo.exibe()
 	inimigo.move()
 
-
+	if (personagem.estaColidindo(inimigo)){
+		noLoop()
+	}
+	
   }
 
   
